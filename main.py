f5f47108091b0ea35ffa1ea74b9d638e78f7bb52
Omar <omarsan786@gmail.com>
2022-11-03 20:41:41 -0400

    User can now guess the word at any point with correct guess ending the game. User experience also updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numbers
 import random
 import hangman_words
 import hangman_art
@@ -9,44 +10,47 @@ chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 guessed_letters = []
 
-# Testing code
-print(f"The word to guess is: {chosen_word}")
-
 display = []
 for _ in range(word_length):
     display += "_"
+print(f"{' '.join(display)}\n")
 
 while not game_is_finished:
 
     guess = input("Guess a letter: ").lower()
 
-    while guess in guessed_letters or len(guess) > 1:
-        if guess in guessed_letters:
-            print(f"You've already guessed the letter '{guess}'")
-            guess = input("Guess a letter: ").lower()
-        elif len(guess) > 1:
-            print(f"'{guess}' is not a valid input. Please only enter single letters")
-            guess = input("Guess a letter: ").lower()
-
-    guessed_letters.append(guess)
-
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-    print(f"{' '.join(display)}")
-
-    if guess not in chosen_word:
-        print(f"You guessed {guess}, that's not in the word. You lose a life.")
-        lives -= 1
-        if lives == 0:
-            game_is_finished = True
-            print("You lose.")
-
-    if not "_" in display:
+    if guess == chosen_word:
         game_is_finished = True
-        print("You win.")
+        print(f"Congratulations! You guessed the word '{chosen_word}' correctly!")
+    else:
 
-    print(hangman_art.stages[lives])
+        while guess in guessed_letters or len(guess) > 1 or guess.isdigit():
+            if guess in guessed_letters:
+                print(f"You've already guessed the letter '{guess}'")
+                guess = input("Guess a letter: ").lower()
+            elif len(guess) > 1 or guess.isdigit():
+                print(f"'{guess}' is not a valid input. Please only enter single letters")
+                guess = input("Guess a letter: ").lower()
 
-    print("Letters you've guessed: " + f"{', '.join(guessed_letters)}")
+        guessed_letters.append(guess)
+
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+        print(f"{' '.join(display)}")
+
+        if guess not in chosen_word:
+            print(f"You guessed {guess}, that's not in the word. You lose a life.")
+            lives -= 1
+            if lives == 0:
+                game_is_finished = True
+                print(f"You lose :( \nThe correct word was '{chosen_word}'!")
+
+        if not "_" in display:
+            game_is_finished = True
+            print(f"Congratulations! You have guessed the word '{chosen_word}' correctly")
+
+        print(hangman_art.stages[lives])
+        print("Letters you've guessed: " + f"{', '.join(guessed_letters)}\n")
+
